794c63ab5a39b1046c8f79a15572ea09228c7dd3
Stefan <stefankollaart@gmail.com>
2018-12-19 16:24:39 +0100

    Updates

---
+++ precache-manifest.3b979792e36e116ea434c5fe9421578d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3fff20173ca04c0b76fdb56c74f50b7",
+    "url": "/trivia/static/media/final-bumper.a3fff201.mp3"
+  },
+  {
+    "revision": "379dda2d44e7523890b5eb8e06ff0b03",
+    "url": "/trivia/static/media/chazia.379dda2d.png"
+  },
+  {
+    "revision": "d9b7ec7ac2785423f792c9eae81672fa",
+    "url": "/trivia/static/media/bumper.d9b7ec7a.mp3"
+  },
+  {
+    "revision": "69d1d5ec7b7d473d342c96efdbbd10ea",
+    "url": "/trivia/static/media/bg.69d1d5ec.mp3"
+  },
+  {
+    "revision": "30964e3390d41baeff0b",
+    "url": "/trivia/static/js/runtime~main.30964e33.js"
+  },
+  {
+    "revision": "7479f696c2d40bf741ce",
+    "url": "/trivia/static/js/main.7479f696.chunk.js"
+  },
+  {
+    "revision": "62f216a9d1376cffe85f",
+    "url": "/trivia/static/js/1.62f216a9.chunk.js"
+  },
+  {
+    "revision": "7479f696c2d40bf741ce",
+    "url": "/trivia/static/css/main.0b71356a.chunk.css"
+  },
+  {
+    "revision": "beacc63c7245454c4b25e64095864a0e",
+    "url": "/trivia/index.html"
+  }
+];
