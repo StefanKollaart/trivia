606080fdb2c482e005cbee1b03b126bc31f41f4e
Stefan <stefankollaart@gmail.com>
2019-04-09 12:11:53 +0200

    questions in app

---
+++ src/questions.js
@@ -0,0 +1,81 @@
+export const questions = [
+  {
+    id: 1,
+    question: "Uit welke film komt deze afbeelding?",
+    answer_1: "The Wolf Of Wall Street",
+    answer_2: "Inside Job",
+    answer_3: "Titanic",
+    right_answer: 1,
+    created_at: "2018-12-19T11:52:40.753Z",
+    updated_at: "2018-12-19T11:52:40.753Z",
+    image: "https://number42.nl/wp-content/uploads/2014/08/R1_V10.jpg"
+  },
+  {
+    id: 2,
+    question: "In welke film redt een ogre een princes uit een toren?",
+    answer_1: "Tangled",
+    answer_2: "Shrek",
+    answer_3: "Toy Story",
+    right_answer: 2,
+    created_at: "2018-12-19T11:54:59.985Z",
+    updated_at: "2018-12-19T11:54:59.985Z",
+    image: null
+  },
+  {
+    id: 3,
+    question: "Wat is 22 * 6",
+    answer_1: "132",
+    answer_2: "136",
+    answer_3: "142",
+    right_answer: 1,
+    created_at: "2018-12-19T12:01:13.306Z",
+    updated_at: "2018-12-19T12:01:13.306Z",
+    image: null
+  },
+  {
+    id: 4,
+    question:
+      "Hoe heet de alert die je krijgt op je mobiele telefoon ter vervanging van het luchtalarm?",
+    answer_1: "Amber Alert",
+    answer_2: "SOS-melding",
+    answer_3: "NL-alert",
+    right_answer: 3,
+    created_at: "2018-12-19T12:01:13.306Z",
+    updated_at: "2018-12-19T12:01:13.306Z",
+    image: null
+  },
+  {
+    id: 5,
+    question: "Welke van deze Queen hits is als eerst uitgebracht?",
+    answer_1: "Killer Queen",
+    answer_2: "Bohemian Rhapsody",
+    answer_3: "Love Of My Life",
+    right_answer: 1,
+    created_at: "2018-12-19T12:01:13.306Z",
+    updated_at: "2018-12-19T12:01:13.306Z",
+    image: null
+  },
+  {
+    id: 6,
+    question: "Van welke bekendheid is dit een jeugdfoto?",
+    answer_1: "Katy Perry",
+    answer_2: "Madonna",
+    answer_3: "Britney Spears",
+    right_answer: 3,
+    created_at: "2018-12-19T12:01:13.306Z",
+    updated_at: "2018-12-19T12:01:13.306Z",
+    image:
+      "https://froot.nl/wp-content/uploads/2013/05/Froot-bekende-mensen-toen-ze-jong-waren-21.jpg"
+  },
+  {
+    id: 7,
+    question: "Uit welk land komt de dans de Sirtaki?",
+    answer_1: "Spanje",
+    answer_2: "Griekenland",
+    answer_3: "Argentinie",
+    right_answer: 2,
+    created_at: "2018-12-19T12:01:13.306Z",
+    updated_at: "2018-12-19T12:01:13.306Z",
+    image: null
+  }
+];
--- src/store.js
+++ src/store.js
@@ -1,133 +1,135 @@
-import { action, observable } from 'mobx'
-import axios from 'axios'
-import bg from './assets/bg.mp3'
-import bumper from './assets/bumper.mp3'
-import finalBumper from './assets/final_bumper.mp3'
+import { action, observable } from "mobx";
+import axios from "axios";
+import bg from "./assets/bg.mp3";
+import bumper from "./assets/bumper.mp3";
+import finalBumper from "./assets/final_bumper.mp3";
+import { questions } from "./questions";
 
 class Store {
-  @observable questions = []
-  @observable playerCount = 0
-  @observable currentQuestion = -1
-  @observable questionsWrong = 0
-  @observable timer = 100
-  @observable timerTotal = 100
-  @observable started = false
-  @observable showImage = false
-  @observable pickedAnswer = -1
-  @observable showAnswers = false
-  @observable audioFile = bumper
-  @observable playButtonPressed = false
-  @observable price = 5000
-  @observable splashscreen = false
-  @observable isCountingDownPlayers = false
+  @observable questions = [];
+  @observable playerCount = 0;
+  @observable currentQuestion = -1;
+  @observable questionsWrong = 0;
+  @observable timer = 100;
+  @observable timerTotal = 100;
+  @observable started = false;
+  @observable showImage = false;
+  @observable pickedAnswer = -1;
+  @observable showAnswers = false;
+  @observable audioFile = bumper;
+  @observable playButtonPressed = false;
+  @observable price = 5000;
+  @observable splashscreen = false;
+  @observable isCountingDownPlayers = false;
 
   @action fetchQuestions() {
-    axios.get('https://www.liquid-paint.nl/quiz.json').then((res) => {
-      this.questions = res.data
-      this.playerCount = res.data.length * 2000 + (Math.floor(Math.random() * 500) + 200)
-    })
+    this.questions = questions;
+    this.playerCount =
+      questions.length * 2000 + (Math.floor(Math.random() * 500) + 200);
   }
 
   @action setAnswer(id) {
-    this.pickedAnswer = id
+    this.pickedAnswer = id;
   }
 
   setPlayButton() {
-    this.playButtonPressed = true
-    this.startQuiz()
+    this.playButtonPressed = true;
+    this.startQuiz();
   }
 
   setTimer(amount) {
-    this.timer = amount * 25
-    this.timerTotal = amount * 25
-    this.startTimer()
+    this.timer = amount * 25;
+    this.timerTotal = amount * 25;
+    this.startTimer();
   }
 
   startTimer() {
     setTimeout(() => {
-      this.timer -= 1
+      this.timer -= 1;
 
       if (this.timer == 0) {
-        this.timerDone()
+        this.timerDone();
       } else {
-        this.startTimer()
+        this.startTimer();
       }
-    }, 30)
+    }, 30);
   }
 
   timerDone() {
     if (this.started) {
-      this.playingTimerDone()
+      this.playingTimerDone();
     } else {
-      this.startQuiz()
+      this.startQuiz();
     }
   }
 
   playingTimerDone() {
     if (this.showImage) {
-      this.showImage = false
-      this.setTimer(10)
+      this.showImage = false;
+      this.setTimer(10);
     } else {
-      this.answers()
+      this.answers();
       setTimeout(() => {
-        this.nextQuestion()
-      }, 5000)
+        this.nextQuestion();
+      }, 5000);
     }
   }
 
   answers() {
-    this.showAnswers = true
-    if (this.pickedAnswer !== this.questions[this.currentQuestion].right_answer) {
-      this.questionsWrong += 1
+    this.showAnswers = true;
+    if (
+      this.pickedAnswer !== this.questions[this.currentQuestion].right_answer
+    ) {
+      this.questionsWrong += 1;
     }
-    if ((this.questions.length) == this.currentQuestion + 1) {
-      this.audioFile = finalBumper
+    if (this.questions.length == this.currentQuestion + 1) {
+      this.audioFile = finalBumper;
     }
-    var losers = Math.floor(Math.random() * 2000) + 1000
-    var newPlayers = this.playerCount - losers
-    var perItem = Math.floor(losers / 200)
-    this.isCountingDownPlayers = true
-    this.countDownLosers(perItem, newPlayers)
+    var losers = Math.floor(Math.random() * 2000) + 1000;
+    var newPlayers = this.playerCount - losers;
+    var perItem = Math.floor(losers / 200);
+    this.isCountingDownPlayers = true;
+    this.countDownLosers(perItem, newPlayers);
   }
 
   countDownLosers(perItem, newPlayers) {
     setTimeout(() => {
-      this.playerCount -= perItem
+      this.playerCount -= perItem;
       if (this.playerCount > newPlayers) {
-        this.countDownLosers(perItem, newPlayers)
+        this.countDownLosers(perItem, newPlayers);
       } else {
-        this.isCountingDownPlayers = false
+        this.isCountingDownPlayers = false;
       }
-    }, 5)
+    }, 5);
   }
 
   startQuiz() {
-    this.started = true
-    this.audioFile = bg
-    this.nextQuestion()
+    this.started = true;
+    this.audioFile = bg;
+    this.nextQuestion();
   }
 
   nextQuestion() {
-    this.showAnswers = false
-    this.pickedAnswer = -1
-    if ((this.questions.length) == this.currentQuestion + 1) {
-      this.finishQuiz()
+    this.showAnswers = false;
+    this.pickedAnswer = -1;
+    if (this.questions.length == this.currentQuestion + 1) {
+      this.finishQuiz();
     } else {
-      this.currentQuestion += 1
+      this.currentQuestion += 1;
       if (this.questions[this.currentQuestion].image != undefined) {
-        this.showImage = true
-        this.setTimer(7)
+        this.showImage = true;
+        this.setTimer(7);
       } else {
-        this.setTimer(10)
+        this.setTimer(10);
       }
     }
   }
 
   finishQuiz() {
-    this.audioFile = finalBumper
-    this.finished = true
-    this.started = false
-    this.splashscreen = true
+    this.audioFile = finalBumper;
+    this.finished = true;
+    this.started = false;
+    this.splashscreen = true;
   }
 }
 
